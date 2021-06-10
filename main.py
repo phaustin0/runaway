f7456297409560fb8096419c94b2225950c350f3
phaustin0 <synterus@gmail.com>
2021-06-10 16:48:45 +0800

    created game class

---
+++ main.py
@@ -0,0 +1,97 @@
+# Main file to handle all game logic
+import pygame
+import sys
+from settings import *
+
+
+# game class
+class Game:
+    def __init__(self):
+        # initialise pygame
+        pygame.init()
+
+        # create screen
+        self.screen = pygame.display.set_mode((width, height))
+        pygame.display.set_caption(title)
+
+        # create clock
+        self.clock = pygame.time.Clock()
+
+        # running variable for whether application is running
+        self.running = True
+
+        # playing variable for whether the game is being played
+        self.playing = False
+
+    # create a new game
+    def new(self):
+        # start playing
+        self.playing = True
+
+        # create sprite groups
+        self.all_sprites = pygame.sprite.LayeredUpdates()
+
+    # event handling
+    def events(self):
+        for event in pygame.event.get():  # check through each event
+            if event.type == pygame.QUIT:  # if user pressed 'X' button
+                # exit the loop
+                self.playing = False
+                self.running = False
+
+    # update game
+    def update(self):
+        self.all_sprites.update()
+
+    # draw stuff
+    def draw(self):
+        # fill screen
+        self.screen.fill(black)
+
+        # draw all sprites on the screen
+        self.all_sprites.draw()
+
+        # make sure everything is running at a constant fps
+        self.clock.tick(fps)
+
+        # update the screen
+        pygame.display.update()
+
+    # main game loop
+    def main(self):
+        while self.playing:
+            self.events()
+            self.update()
+            self.draw()
+        self.running = False
+
+    # intro screen TODO
+    def intro_screen(self):
+        pass
+
+    # game over screen TODO
+    def game_over(self):
+        pass
+
+# check if the file is being run and not imported
+if __name__ == '__main__':
+    # create an instance of the game
+    g = Game()
+
+    # show the intro screen
+    g.intro_screen()
+
+    # prepare the game
+    g.new()
+
+    # main loop
+    while g.running:
+        g.main()
+
+        # once game is done show the game over screen
+        g.game_over()
+
+    # exit out of pygame and quit the program
+    pygame.quit()
+    sys.exit()
+
---
+++ settings.py
@@ -0,0 +1,12 @@
+# settings file
+
+# window settings
+width = 900
+height = 600
+title = 'The Runaway'
+
+# game settings
+fps = 60
+
+# colours
+black = '#000'
